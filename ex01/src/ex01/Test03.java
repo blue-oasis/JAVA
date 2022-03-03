96069927280452e82c3803b244e48dfe1e710aac
지원 <user@DESKTOP-54KUMPJ>
2022-03-03 20:34:30 +0900

    테스트~~

---
+++ ex01/src/ex01/Test03.java
@@ -0,0 +1,18 @@
+package ex01;
+
+public class Test03 {
+	
+		public static int add(int x,int y) {
+		return x + y; 
+		}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 30;
+		System.out.println(a + b);
+		int c = add(a, b);
+		System.out.println(c);
+	}
+}
