3b66670b5d81e097883195a4d70fdfb351f7632b
user <user@DESKTOP-54KUMPJ>
2022-03-03 19:40:03 +0900

    JAVA 첫 프로젝트

---
+++ ex01/src/ex01/test01.java
@@ -0,0 +1,11 @@
+package ex01;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcom!!");
+		System.out.println("자바 세계로 오신 것을 환영합니다.");
+	}
+
+}
