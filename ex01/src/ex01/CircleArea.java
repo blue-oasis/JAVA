ac105b7470a5547cdd2d396aaa807dfceb1b1c62
지원 <user@DESKTOP-54KUMPJ>
2022-03-04 14:22:59 +0900

    원 면적 계산

---
+++ ex01/src/ex01/CircleArea.java
@@ -0,0 +1,14 @@
+package ex01;
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI = 3.14;
+		double radious = 10.0;
+		double circleArea = radious * radious * PI;
+		
+		System.out.println("원의 면적 =" + circleArea);
+	}
+
+}
