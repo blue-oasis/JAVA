25af5a161d729ae6cd2a6a779ed1243b017edb1c
지원 <user@DESKTOP-54KUMPJ>
2022-03-04 16:11:24 +0900

    입력 받기 (형변환)

--- ex01/src/ex01/TypeConversion.java
+++ ex01/src/ex01/TypeConversion.java
@@ -9,7 +9,7 @@ public class TypeConversion {
 		System.out.println(b+i);
 		System.out.println(10/4);
 		System.out.println(10.0/4);
-		System.out.println((char)0x12340041);
+		System.out.println((char)0x12340141);
 		System.out.println((byte)(b+i));
 		System.out.println((int)2.9+1.8);
 		System.out.println((int)(2.9+ 1.8));
