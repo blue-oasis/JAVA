3a9ec4e317f43bfd4c0c3c613ed98230c0e1e8c0
user <user@DESKTOP-54KUMPJ>
2022-03-10 13:47:25 +0900

    JAVA 함수 테스트

---
+++ ex01/src/ex01/fTOcFunction.java
@@ -0,0 +1,14 @@
+package ex01;
+
+public class fTOcFunction {
+	
+		public static void fTOc(double x) {
+			System.out.println((x - 32) * 5/9);
+		}
+		//화씨 섭씨 변환 함수 테스트
+	public static void main(String[] args) {
+		
+		fTOc(100);
+	}
+
+}
