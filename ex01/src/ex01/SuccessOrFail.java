5c81785f5c808e2211c4d58119ebb953e4cf4d92
user <user@DESKTOP-54KUMPJ>
2022-03-10 14:09:49 +0900

    단일 if문

---
+++ ex01/src/ex01/SuccessOrFail.java
@@ -0,0 +1,17 @@
+package ex01;
+import java.util.Scanner;
+
+public class SuccessOrFail {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("점수를 입력하시오: ");
+		int score = scan.nextInt();
+		if (score >= 80)
+			System.out.println("축하합니다! 합격입니다.");
+		scan.close();
+		
+	}
+
+}
