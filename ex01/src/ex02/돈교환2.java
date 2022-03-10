ad6281561dfb03bdaa17dbe7d5cf84e4c2eac1ce
user <user@DESKTOP-54KUMPJ>
2022-03-10 16:23:56 +0900

    교환 문제 개선 %활용

---
+++ ex01/src/ex02/돈교환2.java
@@ -0,0 +1,33 @@
+package ex02;
+import java.util.Scanner;
+public class 돈교환2 {
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.print("금액을 입력하시오.");
+	int won = in.nextInt();
+
+	int 오만 = won / 50000;
+	won %= 50000;
+	int 만 = won / 10000;
+	won %= 10000;
+	int 천 = won / 1000;
+	won %= 1000;
+	int 백 = won / 100;
+	won %= 100;
+	int 오십 = won / 50;
+	won %= 50;
+	int 십 = won / 10;
+	won %= 10;
+	int 일 = won / 1;
+	
+	System.out.println("오만원권 " + 오만 +"매" );
+	System.out.println("만원권 " + 만 +"매" );
+	System.out.println("천원권 " + 천 +"매" );
+	System.out.println("백원권 " + 백 +"개" );
+	System.out.println("오십원 " + 오십 +"개" );
+	System.out.println("십원 " + 십 +"개");
+	System.out.println("일원 " + 일 +"개" );
+	
+	in.close();
+	}	
+}
