f6c30a9b5a0fed655da7526f6097650401d6de15
user <user@DESKTOP-54KUMPJ>
2022-03-11 11:39:02 +0900

    구구단 for 중첩

--- ex01/src/ex02/NestedLoop.java
+++ ex01/src/ex02/NestedLoop.java
@@ -3,8 +3,10 @@ package ex02;
 public class NestedLoop {
 	//구구단 출력
 	public static void main(String[] args) {
-		for(int i = 1; i < 10; i++) { 	// 2단~9단
-			for(int j = 2; j<10; j++) { 	//각 단 출력
+		System.out.print("2단\t3단\t4단\t5단\t6단\t7단\t8단\t9단");
+		System.out.println();
+		for(int i = 1; i < 10; i++) { // 2단~9단
+			for(int j = 2; j<10; j++) { 	//각 단 나란한 줄로 출력
 				System.out.print(j + "*" + i + "="+ i*j);
 				System.out.print('\t');
 			}
