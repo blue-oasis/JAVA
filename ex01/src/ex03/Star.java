dafa6cb3250e4b12fec423821ab658d9f544a548
user <user@DESKTOP-54KUMPJ>
2022-03-11 16:46:28 +0900

    속찬 사각형

---
+++ ex01/src/ex03/Star.java
@@ -0,0 +1,16 @@
+package ex03;
+
+public class Star {
+
+	public static void main(String[] args) {
+		 //속찬 사각형 그리기
+		for(int i = 0; i<5; i++) {
+			for(int j = 0; j<10; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+			
+	}
+
+}
