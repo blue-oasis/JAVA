52d6a75d0fd3b973444a77462a298d2a15074e1e
user <user@DESKTOP-54KUMPJ>
2022-03-11 15:12:13 +0900

    별 피라미드 그리기

---
+++ ex01/src/ex03/별_피라미드.java
@@ -0,0 +1,24 @@
+package ex03;
+
+public class 별_피라미드 {
+
+	public static void main(String[] args) {
+		
+		
+			for(int i = 0; i<5; i++) {
+			
+			for(int j = 0; j<(5*2)-1; j++) {
+			
+				if(i==4||i+j == 4|| j-i==4) { //i4행, j4열 그리고 (1,3)(2,2)(3,1)찍기
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+				
+		}
+		System.out.println();
+
+	}
+	}
+}
