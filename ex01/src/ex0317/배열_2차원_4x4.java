c7d9f07cf8d4e4125666520253e4ca6f9fee33e8
JiWon <radiohead.knou.ac.kr>
2022-03-17 14:08:18 +0900

    랜덤 숫자 10개 넣게 고쳐야함(4x4 배열)

--- ex01/src/ex0317/배열_2차원_4x4.java
+++ ex01/src/ex0317/배열_2차원_4x4.java
@@ -6,20 +6,22 @@ public class 배열_2차원_4x4 {
 		int [][] A = new int[4][4];
 		
 	for(int z = 0; z<10; z++) {
-		for(int i = 0; i<A.length; i++) {
-			for(int j = 0; j<A[i].length; j++) {
-				A[i][j] = (int)(Math.random()*10+1);
-			}
-				
-			}
+		int 행 = (int)(Math.random()*3+1);
+		int 열 = (int)(Math.random()*3+1);
+			if(A[행][열]==0) {
+			A[행][열] = (int)(Math.random()*10+1);
+		}
+			
+	}		
+			
 		
 		for(int i = 0; i<A.length; i++) { //순서대로 출력
 			for(int j = 0; j<A[i].length; j++) {
-		  System.out.print(A[i][j]);
+		  System.out.print(A[i][j] + " ");
 		
 			}
+			System.out.println();
+		}
+	}
+}
 
-}
-	}
-	}
-}
