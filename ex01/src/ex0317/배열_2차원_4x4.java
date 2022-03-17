851d20855b503d33ff8ba1c1a0e954e169983920
JiWon <radiohead.knou.ac.kr>
2022-03-17 14:55:06 +0900

    배열 4X4완성

--- ex01/src/ex0317/배열_2차원_4x4.java
+++ ex01/src/ex0317/배열_2차원_4x4.java
@@ -5,24 +5,24 @@ public class 배열_2차원_4x4 {
 	public static void main(String[] args) {
 		int [][] A = new int[4][4];
 		
-	for(int z = 0; z<10; z++) { //랜덤으로 값 넣어주기
-		int 행 = (int)(Math.random()*4+1) -1; //행, 열 인덱스 랜덤 (0~3)
-		int 열 = (int)(Math.random()*4+1) -1; //0이 기본으로 안나와서 -1붙임
-			if(A[행][열]==0) {
+	for(int z = 0; z<10; z++) { //랜덤으로 값 10회 넣어주기
+		int 행 = (int)(Math.random()*4) ; //행, 열 인덱스 랜덤 (0~3)
+		int 열 = (int)(Math.random()*4) ; 
+			if(A[행][열]==0) { //접근값이 비어있으면 1~10 랜덤할당
 			A[행][열] = (int)(Math.random()*10+1);
 		}
-			else if(A[행][열]!=0) {
+			else if(A[행][열]!=0) { //이미 값이 있으면 z값-1해서 반복 다시
 				z--;
 			}
 			
 	}		
 			
 		
-		for(int i = 0; i<A.length; i++) { //순서대로 출력
+		for(int i = 0; i<A.length; i++) { //배열 순서대로 출력
 			for(int j = 0; j<A[i].length; j++) {
-		  System.out.print(A[i][j] + " ");
+		  System.out.print(A[i][j] + "\t");
 			}
-			System.out.println();
+			System.out.println(); //행마다 줄바꿈
 		}
 	}
 }
