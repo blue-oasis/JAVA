18c1d006b6663d7076382965e18e768bb2e0eb20
blue-oasis <radiohead@knou.ac.kr>
2022-03-23 21:19:37 +0900

    vsc 푸쉬 실험

--- ex01/src/ex0318/AccessEx.java
+++ ex01/src/ex0318/AccessEx.java
@@ -10,7 +10,7 @@ public class AccessEx {
 	public static void main(String[] args) {
 		Sample aClass = new Sample();
 		aClass.a =10; //퍼블릭
-		aClass.b = 10; //프라이빗 맴버라 접근 불가
+		aClass.c = 10; //프라이빗 맴버라 접근 불가
 		aClass.c= 10; //디폴트 (같은 패키지)
 		
 	}
