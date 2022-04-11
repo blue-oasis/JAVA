dec70574c106ae0f08d0366ecfb923988033e910
JiWon <radiohead@knou.ac.kr>
2022-04-11 13:32:59 +0900

    Update 숫자_합_구하기2.java
    리팩토링 - 입력 - 확인1 -입력2 - 확인2 -출력

--- ex01/src/CodingTest/숫자_합_구하기2.java
+++ ex01/src/CodingTest/숫자_합_구하기2.java
@@ -12,27 +12,78 @@ public class 숫자_합_구하기2 {
 			char[] c_sumNum; //더할숫자 문자배열
 			int num; //숫자갯수
 			int sum = 0; //합
-			boolean check = false; //숫자여부
+			boolean check = true; //숫자여부
 			boolean c2 = true; //숫자 갯수, 띄어쓰기 여부 검사
-			boolean check3 = false;
+			boolean check3 = true; //범위 검사
+			boolean allCheck = true;
 					
 			System.out.print("더할 숫자의 개수를 입력해 주세요(1~100): ");
 			num = in.nextInt();
 			do {
-				check3 = false;
+				
 				if(num>0 && num<=100) { //범위 검사
-					c_sumNum = new char[num];
+					check3 = false;
 				}
-				else {
+				if(check3){
 					System.out.print("범위 오류 : 숫자를 다시 입력해 주세요\n");
 					num = in.nextInt();
 					check3 = true;
 				}
 			}while(check3);
+			c_sumNum = new char[num];
 			
 			System.out.print("숫자를 띄어쓰기 없이 입력해 주세요 : ");
 			sumNum = in.next();
 			
+			do { // 입력 2 조건검사
+				
+				if(sumNum.length() == num) { 
+					for(int i = 0; i<c_sumNum.length; i++) {
+						c_sumNum[i] = sumNum.charAt(i);			
+					}
+					c2 = false;
+				}
+				
+				if(c2) {
+					System.out.println("갯수가 일치하지 않거나 공백이 있습니다. 다시 입력하세요");
+					sumNum = in.nextLine();
+					c2 = true;
+				}
+				
+				if(c2 == false){
+					for(int i = 0; i<c_sumNum.length; i++) { //숫자 여부 검사
+						if(c_sumNum[i]>47 && c_sumNum[i]<58) {
+							check = false;
+						}
+						else {
+							check = true;
+							break;
+						}
+					}
+						if(check == true) {
+						System.out.println("문자를 제외하고 숫자만 입력해 주세요");
+							sumNum = in.next();
+							check = true;
+						}
+						
+					}
+				
+				
+				if(check == false && c2 == false) {
+					allCheck = false;
+				}
+				
+				} while(allCheck);
+			
+			//연산
+			for(int i = 0; i<c_sumNum.length; i++) {
+				sum += c_sumNum[i] - 48;	
+			}
+			
+			System.out.println(sum);
+			
+			in.close();	
+		
 			
 			/*while(true) {
 				System.out.print("더할 숫자의 개수를 입력해 주세요(1~100): ");
