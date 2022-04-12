ec718b749b1adb498360d5a42328545da7b302d7
JiWon <radiohead@knou.ac.kr>
2022-04-12 10:21:22 +0900

    외부 패키지의 클래스 사용 방법 2(임포트 구문사용)
    계층구조 클래스 사용

--- ex01/src/ex0412/PackageImport_2.java
+++ ex01/src/ex0412/PackageImport_2.java
@@ -1,11 +1,11 @@
 package ex0412;
 //외부 패키지의 클래스 사용 방법 2(임포트 구문사용) 실습
-import ex0411.ClassA;
+import ex0412.chap07.s1.A; //패키지.클래스 생성시 .으로 구분해 하위경로 생성가능
 public class PackageImport_2 {
 
 	public static void main(String[] args) {
 		//객체 생성
-		ClassA a = new ClassA();
+		A a = new A();
 		//맴버활용
 		System.out.println(a.n);
 		System.out.println(a.m);
