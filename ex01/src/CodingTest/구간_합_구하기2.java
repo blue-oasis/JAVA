171240408f988ad3518751580811258ff36cfdf7
JiWon <radiohead@knou.ac.kr>
2022-04-12 13:44:10 +0900

    Update 구간_합_구하기2.java
    예외처리 추가

--- ex01/src/CodingTest/구간_합_구하기2.java
+++ ex01/src/CodingTest/구간_합_구하기2.java
@@ -17,14 +17,14 @@ public class 구간_합_구하기2 {
 		
 		int[] data = new int[dNum+1];
 
-		System.out.println("데이터를 입력해 주세요(0<= n <=1000) : ");
+		System.out.println("데이터를 입력해 주세요(0 <= n <= 1000) : ");
 		for(int i = 1; i<=dNum; i++) {  //구간합 배열 생성
 			int check = in.nextInt();
-			if((check >= 0) && (check<=1000)) {
+			if((check >= 0) && (check<=1000)) { //범위 검사
 				data[i] = (check) + data[i-1];
 			}
 			else {
-				System.out.println("숫자 범위(0<= n <=1000)를 확인하고 다시 입력해주세요 : ");
+				System.out.println("숫자 범위(0 <= n <= 1000)를 확인하고 다시 입력해주세요 : ");
 				i--;
 			}
 		}
@@ -33,7 +33,12 @@ public class 구간_합_구하기2 {
 		for(int i = 0; i<qNum; i++) { //1 3 입력시 data[3] - data[0]
 			int q = in.nextInt();
 			int q2 = in.nextInt();
-			if((q>0 && q<=dNum) && (q2>0 && q2<=dNum)) {
+			if((q>0 && q<=dNum) && (q2>0 && q2<=dNum)) {//범위 검사
+				if(q>q2) { //사용자가 거꾸로 입력하면 바꾸기
+					int temp = q;
+					q = q2;
+					q2 = temp;
+				}
 				System.out.println(data[q2] - data[q-1]);
 			}
 			else {
