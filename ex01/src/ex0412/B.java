e15a186db2086f1704bd2442af5be173f2bdcd27
JiWon <radiohead@knou.ac.kr>
2022-04-12 14:49:36 +0900

    클래스 import

--- ex01/src/ex0412/B.java
+++ ex01/src/ex0412/B.java
@@ -1,5 +1,5 @@
 package ex0412;
-
+//A 클래스와 같은 패키지
 public class B {
 	public void print() {
 		A a = new A();
--- ex01/src/ex0412/chap07/s1/A.java
+++ ex01/src/ex0412/chap07/s1/A.java
@@ -7,9 +7,6 @@ public class A {
 	public void print() {
 		System.out.println("객체 생성 및 활용");
 	}	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 }
