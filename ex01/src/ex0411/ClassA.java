6eaa5a44a132a88f91027cb2cb19b3760fac612e
JiWon <radiohead@knou.ac.kr>
2022-04-12 09:45:44 +0900

    Create ClassA.java
    실습용 외부 클래스 파일

---
+++ ex01/src/ex0411/ClassA.java
@@ -0,0 +1,18 @@
+package ex0411;
+
+public class ClassA {
+	
+		public int m = 3;
+		public double n;
+		public boolean a;
+		public void print() {
+			System.out.println("객체 생성 및 활용");
+		}
+		
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
