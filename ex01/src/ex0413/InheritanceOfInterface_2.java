897e49a20cf260155c68de6065727081fe00cc80
JiWon <radiohead@knou.ac.kr>
2022-04-13 15:23:32 +0900

    인터페이스 상속 자슥 클래스의 접근 지정자 실습

---
+++ ex01/src/ex0413/InheritanceOfInterface_2.java
@@ -0,0 +1,24 @@
+package ex0413;
+//인터페이스 상속 자슥 클래스의 접근 지정자 실습
+interface In5 {
+	public abstract void abc();
+}
+
+class Cl2 implements In5{
+	public void abc() {
+		
+	}
+}
+
+/*class Cl3 implements In5{
+	void abc() { 상위보다 작은 범위 불가 = defalut 접근지정자 불가
+	}
+}*/
+public class InheritanceOfInterface_2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
