3b35ed82d0b7d2f65a919b8f2daa9b98f9845757
JiWon <radiohead@knou.ac.kr>
2022-04-13 15:23:25 +0900

    인터페이스와 클래스의 상속 키워드 실습

---
+++ ex01/src/ex0413/InheritanceOfInterface_1.java
@@ -0,0 +1,21 @@
+package ex0413;
+//인터페이스와 클래스의 상속 키워드 실습
+
+interface In3 {}
+interface In4 {}
+//단일 상속
+class Ca implements In3{
+	
+}
+//다중 상속
+class Ca1 implements In3, In4{
+	
+}
+public class InheritanceOfInterface_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
