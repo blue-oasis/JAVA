46fb59b0b41df821e753521e766343d69b6768e7
JiWon <radiohead@knou.ac.kr>
2022-06-04 16:22:00 +0900

    Create 수박수박.java
    프로그래머스 문제

---
+++ ex01/src/CodingTest/수박수박.java
@@ -0,0 +1,27 @@
+package CodingTest;
+
+public class 수박수박 {
+	
+	    public static String solution(int n) {
+	        int su = n/2;
+	        String answer = "";
+	        while(su > 0){
+	            answer = answer + "수박";
+	            su--;
+	        }
+	        
+	        if(n%2 == 1) {
+	        	answer = answer + "수";
+	        }
+	        
+	        return answer;
+	    }
+	
+
+	public static void main(String[] args) {
+		String answer = solution(5);
+		System.out.println(answer);
+
+	}
+
+}
