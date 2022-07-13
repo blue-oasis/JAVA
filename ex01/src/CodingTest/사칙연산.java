0b071be31490ef25f9d387332d46224a13ae4a46
JiWon <radiohead@knou.ac.kr>
2022-07-13 15:09:34 +0900

    백준
    테스트~

---
+++ ex01/src/CodingTest/사칙연산.java
@@ -0,0 +1,15 @@
+package CodingTest;
+import java.util.Scanner;
+public class 사칙연산 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
