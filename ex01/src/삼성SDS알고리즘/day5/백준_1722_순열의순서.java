fd05ef98769d9dfd4a7f19cbfa5f80781bc1394e
JiWon <radiohead@knou.ac.kr>
2022-07-22 18:12:05 +0900

    백준_1722_순열의순서 (순열)
    순열 문제

---
+++ ex01/src/삼성SDS알고리즘/day5/백준_1722_순열의순서.java
@@ -0,0 +1,80 @@
+package 삼성SDS알고리즘.day5;
+// 순열 1~N까지의 수 임의 배열한 수열은 N!가지 있음
+
+// 입력 순열 범위 N
+// 둘째 줄 첫범째 수 Q = 1이면 Q번째 수열 출력
+//  Q= 2 이면 순열 입력받아 몇 번째 순열인지 출력
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class 백준_1722_순열의순서 {
+    static int N, Q;
+    static int[] fact, nums; // 팩토리얼
+    static long[][] dp;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("ex01/src/삼성SDS알고리즘/day5/input_1722.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        fact = new int[21];
+
+        fact[0] = 1;
+        for (int i = 0; i <= 20; ++i) {
+            fact[i] = fact[i - 1] * i;
+        }
+
+        N = Integer.parseInt(br.readLine());
+        visited = new boolean[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int command = Integer.parseInt(st.nextToken());
+
+        if (command == 1) {
+            long taget = Long.parseLong(st.nextToken());
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (visited[j] == true) {
+                        continue;
+                    }
+                    if (taget > fact[N - i - 1]) {
+                        taget -= fact[N - i - 1];
+                    } else {
+                        sb.append(j);
+                        sb.append(" ");
+                        visited[j] = true;
+                        break;
+                    }
+                }
+            }
+
+            System.out.println(sb.toString());
+
+        } else if (command == 2) {
+            nums = new int[N];
+            for (int i = 0; i < N; i++) {
+                nums[i] = Integer.parseInt(st.nextToken());
+            }
+            long result = 0;
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < nums[i]; j++) {
+                    if (visited[j] == false) {
+                        result += fact[N - i - 1];
+                    }
+
+                }
+                visited[nums[i]] = true;
+            }
+
+            System.out.println(result + 1);
+        }
+
+    }
+}
