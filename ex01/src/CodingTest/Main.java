8a95ac27f2394c531e9ed42b4b4b3d616f20b6eb
JiWon <radiohead@knou.ac.kr>
2022-08-24 14:30:59 +0900

    백준 풀이튿
    걍 기본 바탕

---
+++ ex01/src/CodingTest/Main.java
@@ -0,0 +1,18 @@
+package CodingTest;
+// 백준 기본틀 (입력)
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		//Scanner in = new Scanner(System.in);
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//StringTokenizer st = new StringTokenizer(br.readLine());
+		//N = Integer.parseInt(br.readLine());
+		//inputA[i] = Long.parseLong(st.nextToken());
+		
+    }
+}
